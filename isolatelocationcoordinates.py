a08ca3bcf00f7e1c617aea47dbb0a79e8242a8f1
clorisshi <110367434+clorisshi@users.noreply.github.com>
2023-03-26 10:15:06 -0700

    Update isolatelocationcoordinates.py

--- isolatelocationcoordinates.py
+++ isolatelocationcoordinates.py
@@ -28,31 +28,3 @@ for coord in flattened:
 final = pd.DataFrame(zip(latitude,longitude))
 print(final)
 final.to_csv("coordinates_2021_december17_december23.csv",index=False)
-
-
-  
-
-
-'''
-What follows is code for past research (to isolate California data).
-
-df=pd.read_csv("hydrated.csv", header=None)
-df=df.iloc[:,[1,6,11]]
-
-df.columns=["date","id","place"]
-split_up = df["place"]
-df[["place_small","place_large"]]=split_up.str.split(',', expand=True)
-df.drop('place', axis=1, inplace=True)
-dict_id=df.set_index('id').to_dict()['place_large']
-print(dict_id)
-
-counter=0
-cal_ids=[]
-for i in dict_id:
-  if dict_id[i] ==" CA":
-    cal_ids.append(i)
-    counter+=1
-print(cal_ids)
-# df1=pd.DataFrame(cal_ids)
-# df1.to_csv('calidonly.csv',index=False)
-'''
